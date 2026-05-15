a7c11dcd446c829341038a96483a6484fb22fa56
unknown <@ngknn.local>
2026-05-15 16:26:28 +0300

    First

---
+++ WillPC/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace WillPC
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

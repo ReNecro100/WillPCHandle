bebf921877ec5e6a379ddc31011b67ee5814cb85
unknown <@ngknn.local>
2026-05-16 15:49:36 +0300

    Сделано нахождение токена, делается вывод сообщения

---
+++ WillPC/GigaChatInteractions.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+
+class GigaChatInteractions
+{
+    private const string TokenUrl = "https://ngw.devices.sberbank.ru:9443";
+    private const string ResponseUrl = "https://gigachat.devices.sberbank.ru";
+    private string secretToken = "MDE5YTc4MTUtYjEwNS03ZDg2LThmMGQtNzYxMjgzYmFhMGRkOmU5N2NmZDI5LWEwYjQtNDMxZC1iYTI0LTQzYTYxODgzMjU2ZQ==";
+
+    public string GetGigaChatToken(string basicAuthToken)
+    {
+        var handler = new HttpClientHandler
+        {
+            ServerCertificateCustomValidationCallback = (sender, cert, chain, errors) => true
+        };
+
+        using var client = new HttpClient(handler);
+        client.BaseAddress = new Uri(TokenUrl);
+
+        var request = new HttpRequestMessage(HttpMethod.Post, "/api/v2/oauth");
+
+        request.Headers.Add("Accept", "application/json");
+        request.Headers.Add("RqUID", "7bdd1a35-c097-4d77-96c1-71438ee10c8c");
+        request.Headers.Add("Authorization", $"Basic {basicAuthToken}");
+
+        request.Content = new StringContent(
+            "scope=GIGACHAT_API_PERS",
+            Encoding.UTF8,
+            "application/x-www-form-urlencoded"
+        );
+
+        using var response = client.Send(request);
+        string resultText = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+        using var jsonDoc = JsonDocument.Parse(resultText);
+
+        if (jsonDoc.RootElement.TryGetProperty("access_token", out JsonElement tokenElement))
+        {
+            return tokenElement.GetString() ?? throw new Exception("Token is null");
+        }
+
+        throw new Exception("access_token not found");
+    }
+    public string GetGigaChatResponse()
+    {
+        var handler = new HttpClientHandler
+        {
+            ServerCertificateCustomValidationCallback = (sender, cert, chain, errors) => true
+        };
+
+        using var client = new HttpClient(handler);
+        client.BaseAddress = new Uri(ResponseUrl);
+
+        var request = new HttpRequestMessage(HttpMethod.Post, "/api/v1/chat/completions");
+
+        request.Headers.Add("Content-Type", "application/json");
+        request.Headers.Add("Accept", "application/json");
+        request.Headers.Add("Authorization", "Bearer " + GetGigaChatToken(secretToken));
+
+        // Ваши данные из --data-raw
+        string jsonData = @"{
+            ""model"": ""GigaChat"",
+            ""messages"": [
+                {
+                    ""role"": ""user"",
+                    ""content"": ""Привет! Как дела?""
+                }
+            ],
+            ""stream"": false,
+            ""repetition_penalty"": 1
+        }";
+
+        request.Content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+        using var response = client.Send(request);
+        string resultText = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+        using var jsonDoc = JsonDocument.Parse(resultText);
+
+        if (jsonDoc.RootElement.TryGetProperty("choices[0].message.content", out JsonElement tokenElement))
+        {
+            return tokenElement.GetString() ?? throw new Exception("Token is null");
+        }
+        throw new Exception("choices[0].message.content");
+    }
+}
--- WillPC/MainWindow.axaml.cs
+++ WillPC/MainWindow.axaml.cs
@@ -1,6 +1,13 @@
 using Avalonia.Controls;
 using System;
+using System.Diagnostics;
 using System.Management;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace WillPC
 {
@@ -9,26 +16,42 @@ namespace WillPC
         public MainWindow()
         {
             InitializeComponent();
-            Yes();
+            //Yes();
+            GetGigachatResponse();
         }
         public void Yes()
         {
             ManagementObjectSearcher cpu = new ManagementObjectSearcher("SELECT * FROM Win32_Processor");
             ManagementObjectSearcher gpu = new ManagementObjectSearcher("SELECT * FROM Win32_VideoController");
+            ManagementObjectSearcher hdd = new ManagementObjectSearcher("SELECT * FROM Win32_LogicalDisk");
 
             foreach (ManagementObject c in cpu.Get())
             {
-                Dat.Text = "Процессор: " + c["Name"] + '\n' + "Количество ядер: " + c["NumberOfCores"] + "Частота: " + c["MaxClockSpeed"] + " МГц" +'\n';
+                Dat.Text = "Процессор: " + c["Name"] + '\n' + "Количество ядер: " + c["NumberOfCores"] + '\n' + "Частота: " + c["MaxClockSpeed"] + " МГц" +'\n';
             }
             foreach (ManagementObject g in gpu.Get())
             {
-                Dat.Text += "Видеокарта: " + g["Name"] + '\n' + "Видеопамять: " + Math.Round(Convert.ToInt32(g["AdapterRAM"]) / Math.Pow(2, 30)) + " Гб"; //Name, Caption, VideoProcessor
+                Dat.Text += "Видеокарта: " + g["Name"] + '\n' + "Видеопамять: " + Math.Round(Convert.ToInt32(g["AdapterRAM"]) / Math.Pow(2, 30)) + " Гб" + '\n'; //Name, Caption, VideoProcessor
+            }
+            foreach (ManagementObject h in hdd.Get())
+            {
+                Dat.Text += $"Место на диске {h["Name"]} " + Math.Round(Convert.ToInt64(h["FreeSpace"]) / Math.Pow(2, 30)) + " Гб" + '\n'; //Name, Caption, VideoProcessor
             }
             //Для получения информации о жестких дисках(SELECT * FROM Win32_LogicalDisk)
 
-            //PerformanceCounter ramCounter = new PerformanceCounter("Memory", "Available MBytes");
-            //Console.WriteLine("Свободная оперативная память: " + ramCounter.NextValue() + " МБ");
-            //Console.ReadLine();
+            PerformanceCounter ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+            Dat.Text+="Свободная оперативная память: " + ramCounter.NextValue() + " МБ" + '\n';
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT Caption FROM Win32_OperatingSystem");
+            foreach (ManagementObject os in searcher.Get())
+            {
+                Dat.Text += RuntimeInformation.OSDescription;
+            }
+        }
+        public void GetGigachatResponse()
+        {
+            GigaChatInteractions gigaChatInteractions = new GigaChatInteractions();
+            Response.Text = "Token: ";
+            Response.Text += gigaChatInteractions.GetGigaChatResponse();
         }
     }
 }

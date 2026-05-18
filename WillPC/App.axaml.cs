e087a7ce012fbf7eeeaebca6bcbbbc20284709aa
unknown <@ngknn.local>
2026-05-18 11:19:40 +0300

    Коррекция окна ПС, улучшение стилей главной страницы, корректировка и создание стилей под страницу карточки

--- WillPC/App.axaml.cs
+++ WillPC/App.axaml.cs
@@ -15,10 +15,16 @@ namespace WillPC
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                desktop.MainWindow = new GamePageWindow();
-            }
+                desktop.MainWindow = new MainWindow();
+                var agreementWindow = new UserAgreementWindow();
+                agreementWindow.Closed += (_, _) =>
+                {
+                    desktop.MainWindow = new MainWindow();
+                    desktop.MainWindow.Show();
+                };
 
-            base.OnFrameworkInitializationCompleted();
+                desktop.MainWindow = agreementWindow;
+            }
         }
     }
 }

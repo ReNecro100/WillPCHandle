11931113d18c69bf6d5b5df8234a5e2cf1496b83
unknown <@ngknn.local>
2026-05-20 17:43:31 +0300

    создана бд

--- WillPC/obj/Debug/net8.0/WillPC.AssemblyInfo.cs
+++ WillPC/obj/Debug/net8.0/WillPC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WillPC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8408d592032647069c6a00b5c157a6766f17bea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c846338ddd21bc5361a2068857672b9784ee6b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WillPC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WillPC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

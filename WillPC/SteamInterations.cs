63555b7315aeb018fcb61c1e9f3a4a0540522fca
unknown <@ngknn.local>
2026-05-21 14:41:21 +0300

    Объединение всего в один класс

--- WillPC/SteamInterations.cs
+++ WillPC/SteamInterations.cs
@@ -12,40 +12,30 @@ using WillPC;
 using System.IO;
 using System.Net.Http;
 
-class AppCardInfo
-{
-    public int Id { get; set; } //Айдишник отображаемого на главном экране приложения
-    public string Name { get; set; } //Название приложения
-    public string Image { get; set; } //Превьюшка приложения
-    public string Genre { get; set; } //Жанр игры
-    public string CompatibilityIndicator { get; set; } //Индикатор совместимости (ЗЕЛЁНЫЙ, ЖЁЛТЫЙ, КРАСНЫЙ, СЕРЫЙ)
-
-    public AppCardInfo(int id, string name, string image, string genre,string compatibilityIndicator)
-    {
-        Id = id;
-        Name = name;
-        Image = image;
-        Genre = genre;
-        CompatibilityIndicator = compatibilityIndicator;
-    }
-}
-class AppTotalInfo
+class GameInfo
 {
     public int Id { get; set; } //Айдишник отображаемого приложения
     public string HeaderImage { get; set; } //Главный скрин
     public string Name { get; set; } //Название
     public string Description { get; set; } //Описание
+    public string Genre { get; set; } //Жанр игры
     public string MinimalRequirements { get; set; } //Минимальные требования
     public string RecommendedRequirements { get; set; } //Рекомендованные требования
+    public string CompatibilityIndicator { get; set; } //Индикатор совместимости (ЗЕЛЁНЫЙ, ЖЁЛТЫЙ, КРАСНЫЙ, СЕРЫЙ)
     public List<string> Screenshots { get; set; } //Скрины
-    public AppTotalInfo(int id, string headerImage, string name, string description, string minimalRequirements, string recommendedRequirements, List<string> screenshots)
+    public GameInfo(int id, 
+        string headerImage, string name, string description, 
+        string genre, string minimalRequirements, string recommendedRequirements, 
+        string compatibilityIndicator, List<string> screenshots)
     {
         Id = id;
         HeaderImage = headerImage;
         Name = name;
         Description = description;
+        Genre = genre;
         MinimalRequirements = minimalRequirements;
         RecommendedRequirements = recommendedRequirements;
+        CompatibilityIndicator = compatibilityIndicator;
         Screenshots = screenshots;
     }
 }
@@ -55,86 +45,85 @@ class SteamInterations
         Trace.Listeners.Add(new TextWriterTraceListener("log_steam.txt"));
         Trace.AutoFlush = true;
     }
-    public async Task<List<AppCardInfo>> GetFeaturedAppsList()
+    public async Task<List<GameInfo>> GetFeaturedAppsList()
     {
         //https://feed.nuget.org/packages/SteamStorefrontAPI
-        List<AppCardInfo>? featuredGames = ManagerJSON.DeSerialize<AppCardInfo>("cache/featuredGames.json");
+        List<GameInfo>? featuredGames = ManagerJSON.DeSerialize<GameInfo>("cache/gamesList.json");
         if (featuredGames is null)
         {
             FeaturedApps featured = await Featured.GetAsync();
             List<AppInfo> apps = featured.FeaturedWin;
 
-            featuredGames = new List<AppCardInfo>();
+            featuredGames = new List<GameInfo>();
             foreach (var item in apps)
             {
-                SteamApp app = await AppDetails.GetAsync(item.Id);
-
-                HardWareInteractons hardWareInteractons = new HardWareInteractons();
-                GigaChatInteractions gigaChatInteractions = new GigaChatInteractions();
-                
-                string minimalRequirements = Regex.Replace(app.PcRequirements.Minimum is null ? "No requirements" : app.PcRequirements.Minimum.Replace("<li>", "\n"), "<[^>]*>", "");
-                string recommendedRequirements = Regex.Replace(app.PcRequirements.Recommended is null ? "No requirements" : app.PcRequirements.Recommended.Replace("<li>", "\n"), "<[^>]*>", "");
-                string compatibilityIndicator = gigaChatInteractions.GetGigaChatResponse(
-                    "Тебе нужно определить, подходит ли мой компьютер под минимальные или рекомендованные требования игры:\n" +
-                    $"Конфигурация моего компьютера: {hardWareInteractons.GetPCData()}\n" +
-                    $"Минимальные требования игры: {minimalRequirements}\n" +
-                    $"Рекомендованные требования игры: {recommendedRequirements}\n" +
-                    $"Формат ответа - только одно из четырёх слов:\n" +
-                    $"ЗЕЛЁНЫЙ - совместимость и с минимальными, и с рекомендованными требованиями игры\n" +
-                    $"ЖЁЛТЫЙ - совместимость только с минимальными требованиями игры\n" +
-                    $"КРАСНЫЙ - отсутствие совместимости\n" +
-                    $"СЕРЫЙ - требования не заданы" +
-                    "Если отсутствуют либо минимальные, либо рекомендованные требования, оценку совместимости проводи по тем требованиям, которые имеются. В таком случае ЗЕЛЁНЫЙ будет означать совместимость, а КРАСНЫЙ - её отсутствие\n"
-                );
-
-                AppCardInfo featuredGame = new AppCardInfo(
-                    item.Id, 
-                    item.Name, 
-                    item.LargeCapsuleImage, 
-                    app.Genres[0].ToString(),
-                    compatibilityIndicator);
-                featuredGames.Add(featuredGame);
+                await GetGameInfo(item.Id);
             }
             RefreshFeaturedAppsList(featuredGames);
         }
         return featuredGames;
     }
-    public void RefreshFeaturedAppsList(List<AppCardInfo> FeaturedAppsList)
+    public void RefreshFeaturedAppsList(List<GameInfo> FeaturedAppsList)
     {
-        ManagerJSON.Serialize<AppCardInfo>(FeaturedAppsList, "cache/featuredGames.json");
+        ManagerJSON.Serialize<GameInfo>(FeaturedAppsList, "cache/gamesList.json");
     }
-    public async Task<AppTotalInfo> GetAppTotalInfo(int gameId)
+    public async Task<GameInfo> GetGameInfo(int gameId)
     {
         try
         {
-            return ManagerJSON.DeSerialize<AppTotalInfo>($"cache/steamGame_{gameId}.json")[0];
+            foreach (var item in ManagerJSON.DeSerialize<GameInfo>("cache/gamesList.json"))
+            {
+                if (item.Id == gameId)
+                {
+                    return item;
+                }
+            }
+            throw new Exception("not found");
         }
         catch
         {
             SteamApp app = await AppDetails.GetAsync(gameId);
+
+            //Получение индикатора
+            HardWareInteractons hardWareInteractons = new HardWareInteractons();
+            GigaChatInteractions gigaChatInteractions = new GigaChatInteractions();
+
+            string minimalRequirements = Regex.Replace(app.PcRequirements.Minimum is null ? "No requirements" : app.PcRequirements.Minimum.Replace("<li>", "\n"), "<[^>]*>", "");
+            string recommendedRequirements = Regex.Replace(app.PcRequirements.Recommended is null ? "No requirements" : app.PcRequirements.Recommended.Replace("<li>", "\n"), "<[^>]*>", "");
+            string compatibilityIndicator = gigaChatInteractions.GetGigaChatResponse(
+                "Тебе нужно определить, подходит ли мой компьютер под минимальные или рекомендованные требования игры:\n" +
+                $"Конфигурация моего компьютера: {hardWareInteractons.GetPCData()}\n" +
+                $"Минимальные требования игры: {minimalRequirements}\n" +
+                $"Рекомендованные требования игры: {recommendedRequirements}\n" +
+                $"Формат ответа - только одно из четырёх слов:\n" +
+                $"ЗЕЛЁНЫЙ - совместимость и с минимальными, и с рекомендованными требованиями игры\n" +
+                $"ЖЁЛТЫЙ - совместимость только с минимальными требованиями игры\n" +
+                $"КРАСНЫЙ - отсутствие совместимости\n" +
+                $"СЕРЫЙ - требования не заданы" +
+                "Если отсутствуют либо минимальные, либо рекомендованные требования, оценку совместимости проводи по тем требованиям, которые имеются. В таком случае ЗЕЛЁНЫЙ будет означать совместимость, а КРАСНЫЙ - её отсутствие\n"
+            );
+
+            //Получение скринов
             List<string> screenshots = new List<string>();
-            GetAppScreenshots(app.HeaderImage, gameId, 0);
+            await GetAppScreenshots(app.HeaderImage, gameId, 0);
             for (int i = 0; i < 3; i++)
             {
                 await GetAppScreenshots(app.Screenshots[i].PathFull, gameId, i+1);
                 screenshots.Add($"cache/{gameId}_{i+1}.jpg");
             }
-            AppTotalInfo game = new AppTotalInfo(
+            GameInfo game = new GameInfo(
                 gameId,
                 $"cache/{gameId}_0.jpg",
                 app.Name,
                 app.ShortDescription,
+                app.Genres[0].ToString(),
                 Regex.Replace(app.PcRequirements.Minimum is null ? "No requirements" : app.PcRequirements.Minimum.Replace("<li>", "\n"), "<[^>]*>", ""),
                 Regex.Replace(app.PcRequirements.Recommended is null ? "No requirements" : app.PcRequirements.Recommended.Replace("<li>", "\n"), "<[^>]*>", ""),
+                compatibilityIndicator,
                 screenshots
                 );
-            RefreshAppTotalInfo(game);
             return game;
         }
-    }
-    public void RefreshAppTotalInfo(AppTotalInfo app)
-    {
-        ManagerJSON.Serialize<AppTotalInfo>(new List<AppTotalInfo> () { app }, $"cache/steamGame_{app.Id}.json");
     }
     public async Task GetAppScreenshots(string fileURL, int gameId, int screenshotId)
     {
--- WillPC/obj/Debug/net8.0/WillPC.AssemblyInfo.cs
+++ WillPC/obj/Debug/net8.0/WillPC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WillPC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ca564a26e1c14c242f8c9ed886331c070b70962")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e44c6991cb82f55a800d303f47e09e3ad639a740")]
 [assembly: System.Reflection.AssemblyProductAttribute("WillPC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WillPC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

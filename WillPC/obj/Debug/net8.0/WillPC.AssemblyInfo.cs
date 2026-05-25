661770183217ad1668f6308561d932d06091eb1c
ReNecro100 <jarabotaju2007@gmail.com>
2026-05-25 09:34:49 +0300

    Сделал диаграммку :)

--- WillPC/obj/Debug/net8.0/WillPC.AssemblyInfo.cs
+++ WillPC/obj/Debug/net8.0/WillPC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WillPC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e4fb078e1c1867f0c385c21e8fe4fb9a2c03dee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b7a1cd72392ac959f4824acd09095bf464cb304")]
 [assembly: System.Reflection.AssemblyProductAttribute("WillPC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WillPC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
